e02f79135b820a3ca923c3a2d4e38d512cb9a024
divyamzzz <101924382+divyamzzz@users.noreply.github.com>
2024-09-10 13:15:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Chatbot from "./chatbot"; // Import the Chatbot component
 function App() {
   return (
     <div className="App" style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <h1>Ticket Booking Chatbot</h1>
+    
       <Chatbot /> {/* Call the Chatbot component */}
     </div>
   );
